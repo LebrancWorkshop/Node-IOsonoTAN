67f3d2640bd9e7d1f1398028365763382a2cba19
lebrancconvas <lebrancconvas@gmail.com>
2022-08-25 08:48:06 +0700

    ✨ feat: create server with post route

--- index.js
+++ index.js
@@ -1,18 +1,64 @@
-const http = require('http');
+// const http = require('http');
+const fastify = require('fastify');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-const HOSTNAME = process.env.HOSTNAME;
-const PORT = process.env.PORT;
+const HOSTNAME = process.env.HOSTNAME || "localhost";
+const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-	console.log("Create Server.");
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end("Hello Server.");
+let result;
+
+const app = fastify({
+	logger: true
 });
 
-server.listen(PORT, HOSTNAME, () => {
-	console.log(`[SUCCESS] Server running at http://${HOSTNAME}:${PORT}/`);
+// app.get('/', async(request, response) => {
+// 	response.send(result);
+// });
+
+// app.post('/users', async(request, response) => {
+// 	console.log(`Request: ${request.body}`);
+// 	const data = request.body;
+// 	result = {...data};
+// 	response.send(result);
+// });
+
+const usersRoute = {
+	method: "POST",
+	url: "/users",
+	schema: {
+		body: {
+			username: {
+				type: "string"
+			},
+			password: {
+				type: "string"
+			}
+		},
+		response: {
+			200: {
+				type: 'object',
+				properties: {
+					userID: {
+						type: "string"
+					},
+					username: {
+						type: "string"
+					}
+				}
+			}
+		}
+	},
+	handler: async(req, res) => {
+		const requestBody = req.body;
+		result = {...requestBody, isSuccess: true};
+		res.send({...result, userID: "01"});
+	}
+};
+
+app.route(usersRoute);
+
+app.listen(PORT, HOSTNAME, () => {
+	console.log(`[SUCCESS] Server is running at http://${HOSTNAME}:${PORT}`);
 });
