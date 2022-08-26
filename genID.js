dbc779709bb67677b09df0f47da659950da6f1be
lebrancconvas <lebrancconvas@gmail.com>
2022-08-26 21:11:15 +0700

    ✅ test: update test from value expected to length expented

--- genID.js
+++ genID.js
@@ -2,8 +2,8 @@ const Chance = require('chance');
 const chance = new Chance();
 
 const idGenerated = () => {
-	return "Gawae";
-	// return chance.string({ length: 10 });
+	// return "Gawae";
+	return chance.string({ length: 10 });
 };
 
 module.exports = {
--- genID.test.js
+++ genID.test.js
@@ -3,5 +3,5 @@ const genID = require('./genID');
 test('should return string value', () => {
 	const result = genID.idGenerated();
 
-	expect(result).toBe("Username"); 
+	expect(result).toHaveLength(10);
 });
